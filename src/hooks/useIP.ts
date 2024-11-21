6a63b3ba7439be5d5ecddb065d1c2aaafb503590
afolabi-ola <148984714+afolabi-ola@users.noreply.github.com>
2024-11-21 15:54:17 +0100

    Update useIP.ts

--- src/hooks/useIP.ts
+++ src/hooks/useIP.ts
@@ -1,1 +1,10 @@
+import { useContext } from 'react';
+import { IpContext } from '../contexts/IpContext';
 
+export function useIP() {
+  const context = useContext(IpContext);
+  if (!context) {
+    throw new Error('useIP must be used within an IpProvider');
+  }
+  return context;
+}
